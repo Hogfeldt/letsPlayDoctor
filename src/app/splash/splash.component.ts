f3dd0aed8d53c4e9c07acee9629e44319f92a05e
Per Høgfeldt <hogfeldt38@gmail.com>
2020-11-28 13:49:41 +0100

    addod start button

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -12,12 +12,12 @@ export class SplashComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  ngAfterViewInit() {
+  /*ngAfterViewInit() {
     const logo = document.querySelectorAll("#logo path");
     for (let i = 0; i < logo.length; i++) {
       console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
     }
-  };
+  };*/
 }
 
 
